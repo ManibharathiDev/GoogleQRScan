79bfb06c08f086948702d8737dba0ff2192b5a11
01040025 <manibharathir@sugunafoods.com>
2024-06-27 10:26:05 +0530

    Initial Commits

---
+++ app/src/main/java/com/example/googleqrscanner/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.googleqrscanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnTakePicture, btnScanBarcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnTakePicture.setOnClickListener(this);
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btnTakePicture){
+            startActivity(new Intent(MainActivity.this, PictureBarcodeActivity.class));
+        }
+        else{
+            startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+        }
+
+
+    }
+}
